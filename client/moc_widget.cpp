2ddc3015ecab21c61afeb4f2644cc5ee63a3c6fd
Donghyeon0915 <dongdong0915_@naver.com>
2022-12-14 18:32:00 +0900

    UPDATE : moc_widget

--- client/moc_widget.cpp
+++ client/moc_widget.cpp
@@ -1,23 +1,26 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'widget.h'
 **
-** Created by: The Qt Meta Object Compiler version 67 (Qt 5.5.1)
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.3)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
+#include <memory>
 #include "widget.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'widget.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 67
-#error "This file was generated using the moc from 5.5.1. It"
+#error "This file was generated using the moc from 5.15.3. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
 
 QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_Widget_t {
     QByteArrayData data[7];
     char stringdata0[108];
@@ -47,7 +50,7 @@ QT_MOC_LITERAL(6, 75, 32) // "on_messageLineEdit_returnPressed"
 static const uint qt_meta_data_Widget[] = {
 
  // content:
-       7,       // revision
+       8,       // revision
        0,       // classname
        0,    0, // classinfo
        5,   14, // methods
@@ -77,8 +80,8 @@ static const uint qt_meta_data_Widget[] = {
 void Widget::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
     if (_c == QMetaObject::InvokeMetaMethod) {
-        Widget *_t = static_cast<Widget *>(_o);
-        Q_UNUSED(_t)
+        auto *_t = static_cast<Widget *>(_o);
+        (void)_t;
         switch (_id) {
         case 0: _t->readyRead(); break;
         case 1: _t->connected(); break;
@@ -88,13 +91,17 @@ void Widget::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void
         default: ;
         }
     }
-    Q_UNUSED(_a);
+    (void)_a;
 }
 
-const QMetaObject Widget::staticMetaObject = {
-    { &QWidget::staticMetaObject, qt_meta_stringdata_Widget.data,
-      qt_meta_data_Widget,  qt_static_metacall, Q_NULLPTR, Q_NULLPTR}
-};
+QT_INIT_METAOBJECT const QMetaObject Widget::staticMetaObject = { {
+    QMetaObject::SuperData::link<QWidget::staticMetaObject>(),
+    qt_meta_stringdata_Widget.data,
+    qt_meta_data_Widget,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
 
 
 const QMetaObject *Widget::metaObject() const
@@ -104,9 +111,9 @@ const QMetaObject *Widget::metaObject() const
 
 void *Widget::qt_metacast(const char *_clname)
 {
-    if (!_clname) return Q_NULLPTR;
+    if (!_clname) return nullptr;
     if (!strcmp(_clname, qt_meta_stringdata_Widget.stringdata0))
-        return static_cast<void*>(const_cast< Widget*>(this));
+        return static_cast<void*>(this);
     return QWidget::qt_metacast(_clname);
 }
 
@@ -126,4 +133,5 @@ int Widget::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     }
     return _id;
 }
+QT_WARNING_POP
 QT_END_MOC_NAMESPACE
